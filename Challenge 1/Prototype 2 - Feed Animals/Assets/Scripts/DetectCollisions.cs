0bfdd18b824cfca7b40cef4dfa32b93d76749006
Cole <coleispro@hotmail.com>
2020-10-26 08:32:52 -0600

    Final Touches Prototype 2

--- Challenge 1/Prototype 2 - Feed Animals/Assets/Scripts/DetectCollisions.cs
+++ Challenge 1/Prototype 2 - Feed Animals/Assets/Scripts/DetectCollisions.cs
@@ -16,7 +16,7 @@ public class DetectCollisions : MonoBehaviour
         
     }
     
-    void OnTriggerEnet(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
         Destroy(other.gameObject);
