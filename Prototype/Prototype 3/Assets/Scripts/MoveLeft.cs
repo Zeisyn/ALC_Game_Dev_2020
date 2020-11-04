b67220c8cbda66f52d90a1d17c7d215c772b4c3a
Cole <coleispro@hotmail.com>
2020-11-04 09:05:59 -0700

    Finished 3.1

---
+++ Prototype/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 15;
+   
+    
+    
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
--- Prototype/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce;
-    public float gravityModifier;
+    public float jumpForce = 7;
+    public float gravityModifier = 1;
+    public bool isOnGround = true;
     
-    // Start is called before the first frame update
+    
+
     void Start()
     {
         playerRb = GetComponent <Rigidbody>();
         Physics.gravity *= gravityModifier;
     }
 
-    // Update is called once per frame
+   
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
