69c303e00d48685a85d4e0e0790bd103148346c8
Cole <coleispro@hotmail.com>
2020-11-24 09:08:49 -0700

    start Lab 3

---
+++ Prototype/Lab 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+public float horizontalInput;
+public float verticalInput;
+public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move player left /right on horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        //Move player forward/backwards on VerticalInput
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+    }
+}
+
