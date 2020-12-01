1d2f9b28d1a82278006ab880c1fee0fd5818f270
Cole <coleispro@hotmail.com>
2020-12-01 09:08:38 -0700

    LAB 3 Finished

--- Prototype/Lab 3/Assets/Scripts/PlayerController.cs
+++ Prototype/Lab 3/Assets/Scripts/PlayerController.cs
@@ -8,15 +8,36 @@ public class PlayerController : MonoBehaviour
 public float horizontalInput;
 public float verticalInput;
 public float speed = 10.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+public float xRange = 5.0f;
+public float zRange = 5.0f;
+    
     // Update is called once per frame
     void Update()
     {
+        // Keeps player in bounds - left side
+        if (transform.position.x < -xRange) 
+        {
+        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        
+        //Keeps player in bounds - right side
+        if (transform.position.x > xRange) 
+        {
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //Keeps player in bounds - top 
+        if (transform.position.z > zRange) 
+        {
+        transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        //Keeps player in bounds - bottom
+        if (transform.position.z < -zRange) 
+        {
+        transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        
         //Move player left /right on horizontal input
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
--- Prototype/Prototype 3/Assets/Challenge 3/Scripts/MoveLeftX.cs
+++ Prototype/Prototype 3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveLeftX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 10;
     private PlayerControllerX playerControllerScript;
     private float leftBound = -10;
 
